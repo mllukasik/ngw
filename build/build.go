ebd94347cc922da344ca5f25c7f3bac898eae64d
mllukasik <tvpozytyw@gmail.com>
2025-03-12 03:58:41 +0100

    fixes

--- build/build.go
+++ build/build.go
@@ -2,8 +2,8 @@ package build
 
 import "time"
 
-const version = "v1.0.1"
-const buildDate = "Wed, 12 Mar 2025 02:55:32 +0000"
+const version = "${version}"
+const buildDate = "${buildDate}"
 
 type build struct {
 	Version string
