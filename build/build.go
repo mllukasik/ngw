0f3d03a97001265976d01c7f4206b498bd747929
github-action <tvpozytyw@gmail.com>
2025-03-12 02:59:29 +0000

    prepare release

--- build/build.go
+++ build/build.go
@@ -2,8 +2,8 @@ package build
 
 import "time"
 
-const version = "${version}"
-const buildDate = "${buildDate}"
+const version = "v1.0.1"
+const buildDate = "Wed, 12 Mar 2025 02:59:29 +0000"
 
 type build struct {
 	Version string
