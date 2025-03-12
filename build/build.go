1e70e2d83b89fe881be1bb8e2c4a75b54159bd6d
mllukasik <tvpozytyw@gmail.com>
2025-03-12 03:39:14 +0100

    prepare release

--- build/build.go
+++ build/build.go
@@ -16,11 +16,11 @@ func instance() build {
 	versionValue := version
 	buildDateValue := time.Now()
 
-	if version == "${version}" {
+	if version == "v1.0.11" {
 		versionValue = "snapshot"
 		debugValue = true
 	}
-	if buildDate != "${buildDate}" {
+	if buildDate != "Wed, 12 Mar 2025 03:39:14 +0100" {
 		v, err := time.Parse(time.RFC1123Z, buildDate)
 		if err != nil {
 			buildDateValue = v
