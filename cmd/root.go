d521b4b5b622424ebb44505b09fdd3a7938d963d
mllukasik <41591668+mllukasik@users.noreply.github.com>
2025-03-12 01:24:01 +0100

    makefile add prepare-relese (#3)

--- cmd/root.go
+++ cmd/root.go
@@ -1,26 +1,41 @@
 package cmd
 
 import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/mllukasik/ngw/build"
 	"github.com/mllukasik/ngw/cmd/branch"
 	"github.com/mllukasik/ngw/cmd/push"
 	"github.com/spf13/cobra"
 )
 
-func rootCmd(version string) *cobra.Command {
-	return &cobra.Command{
-		Use:   "ngw",
-		Short: "Needless Git Wrapper",
-		Run: func(cmd *cobra.Command, args []string) {
-			cmd.Help()
-		},
-		Version: version,
+func longDesc() string {
+	var builder strings.Builder
+	builder.WriteString("Needless Git Wrapper\n")
+	builder.WriteString(fmt.Sprintf("Version: %s\n", build.Build().Version))
+	if build.Build().Debug {
+		builder.WriteString(fmt.Sprintf("Build date: %s\n", build.Build().Date.Format(time.ANSIC)))
 	}
-
+	return builder.String()
 }
 
-func Execute(version string) error {
-	var root = rootCmd(version)
-	root.AddCommand(branch.BranchCmd)
-	root.AddCommand(push.PushCmd)
-	return root.Execute()
+var rootCmd = &cobra.Command{
+	Use:   "ngw",
+	Short: "Needless Git Wrapper",
+	Long:  longDesc(),
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+	Version: build.Build().Version,
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	rootCmd.AddCommand(branch.BranchCmd)
+	rootCmd.AddCommand(push.PushCmd)
 }
